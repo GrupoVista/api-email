2c0777e15b6c63a84e8f998e650feb74e1d9bb47
Gabriel Souza <gabriel@vistaonline.com.br>
2019-08-21 18:01:18 -0300

    a

--- build.py
+++ build.py
@@ -2,5 +2,5 @@ import docker
 
 client = docker.from_env()
 
-client.images.build(path=".",tag="grupovista/api-email:0.0.3")
-client.images.push(repository="grupovista/api-email", tag="0.0.3")
+client.images.build(path=".",tag="grupovista/api-email:0.0.4")
+client.images.push(repository="grupovista/api-email", tag="0.0.4")
